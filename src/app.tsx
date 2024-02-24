676755fead8670839fcad70ffa4592692a734d8d
Luana Silva <luanasilva.developer@gmail.com>
2024-02-24 18:10:39 -0300

    feat: create and implement add task function

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,55 @@
+import { FormEvent, useState } from "react"
+
+interface Tasks {
+  id: number
+  title: string,
+  isDone: boolean
+}
+
 export function App() {
+  const [title, setTitle] = useState("")
+  const [tasks, setTasks] = useState<Tasks[]>([])
+
+  function handleAddTask(e: FormEvent) {
+    e.preventDefault()
+
+    const newTask = [
+      ...tasks,
+      {
+        id: new Date().getTime(),
+        title: title,
+        isDone: false,
+      }
+    ]
+
+    setTasks(newTask)
+    setTitle("")
+  }
+
   return (
     <>
-      <h1>Hello World!</h1>
+      <header>
+        <h1>TODO</h1>
+      </header>
+
+      <form onSubmit={handleAddTask}>
+        <input
+          type="text"
+          value={title}
+          onChange={e => setTitle(e.target.value)}
+        />
+        <button>Criar</button>
+      </form>
+
+      <main>
+        <ul>
+          {
+            tasks.map(task => (
+              <li>{task.title}</li>
+            ))
+          }
+        </ul>
+      </main>
     </>
   )
 }
