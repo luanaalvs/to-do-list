d2b9172b69de4657a1f951992e41dea613d9ae3a
Luana Silva <luanasilva.developer@gmail.com>
2024-04-14 20:39:10 -0300

    chore: configure absolute imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
+      "@assets": resolve(__dirname, "src/assets"),
+      "@components": resolve(__dirname, "src/components"),
+      "@form": resolve(__dirname, "src/components/form"),
+      "@list": resolve(__dirname, "src/components/list"),
+      "@styles": resolve(__dirname, "src/styles"),
+    },
+  },
+});
